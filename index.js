31be94d49bf4fb06c21d77860da5069d27b1a94e
agilles1 <adam.g.tpt@gmail.com>
2021-02-27 20:08:48 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Add your functions here
+function map(array, f) {
+    let m = []
+    for (let i = 0; i < array.length; i++ ) {
+        m.push(f(array[i]))
+    }
+    return m
+  }
+
+function reduce(array, f, start) {
+    let s = (!!start) ? start : array[0]
+    let i = (!!start) ? 0 : 1
+
+    for (i; i < array.length; i++ ) {
+       s = f(array[i], s)
+    }
+    return s
+}
+
